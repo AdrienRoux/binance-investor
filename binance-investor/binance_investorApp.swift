21d4b7036440396389a561535e1454bf19014fa1
Ad <adrienroux94@gmail.com>
2021-03-26 22:23:47 +0100

    first commit

---
+++ binance-investor/binance_investorApp.swift
@@ -0,0 +1,17 @@
+//
+//  binance_investorApp.swift
+//  binance-investor
+//
+//  Created by Adri on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct binance_investorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
