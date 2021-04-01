8b64e4ec6f8362691e263a606ea3226cc55637a2
Ad <adrienroux94@gmail.com>
2021-04-01 10:17:39 +0200

    Mise à jour de la page d'accueil pour les infos récupérées

--- binance-investor/ContentView.swift
+++ binance-investor/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userKey = UserDefaults.standard.string(forKey: "userKey") ?? ""
+    @State private var userSecret = UserDefaults.standard.string(forKey: "userSecret") ?? ""
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(spacing: 30) {
+                
+                if (userKey == "" || userSecret == ""){
+                    NavigationLink("Enter Credentials", destination: ProfileView(userKey: $userKey, userSecret: $userSecret))
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.yellow)
+                        .cornerRadius(10.0)
+                } else {
+                    VStack(spacing: 30) {
+                        Text("Logged in !")
+                        Text("Api Key : " + UserDefaults.standard.string(forKey: "userKey")!)
+                        Text("Api Secret : " + UserDefaults.standard.string(forKey: "userSecret")!)
+                    }
+                }
+            }
+            .multilineTextAlignment(.center)
+            .navigationBarItems(trailing:
+                                    NavigationLink(destination: ProfileView(userKey: $userKey, userSecret: $userSecret)) {
+                    Image(systemName: "person.crop.circle")
+                        .imageScale(.large)
+                        .accessibilityLabel("User profile")
+                        .padding()
+                    
+                })
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
